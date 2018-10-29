8ac63faca7eeb20372bc929568d95e0c0452a1b2
Austin Downing <HanSolo552@gmail.com>
2018-10-29 09:15:29 -0400

    Add files via upload

---
+++ Change Address.ps1
@@ -0,0 +1,10 @@
+﻿#Sets address
+$Address = "123 Test Ave"
+#Set Users to Modify
+$UsersToModify = Get-ADUser -Filter * -SearchBase "OU=Sub Department, OU=Department ,OU=Users,DC=Contoso,DC=COM" | Select SamAccountName -ExpandProperty SamAccountName
+
+#For Loop to Modify Each User
+
+foreach ($UserToModify in $UsersToModify) {
+set-aduser -identity $UserToModify -StreetAddress $Address
+}
